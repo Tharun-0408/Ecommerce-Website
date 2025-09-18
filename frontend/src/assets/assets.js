5421b7b7864ff1a2f975453e571b1c8c7c0e3fee
Tharun Reddy <tharun.guntuka0408@gmail.com>
2025-09-18 14:11:58 +0530

    safe point

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,8 +1,18 @@
 import logo from "./logo.png";
+import search from "./search.svg";
+import person from "./person.svg";
+import bag from "./bag.svg";
+import list from "./list.svg";
+import dropdown from "./chevron-left.svg"
 
 
 
 
 export const assets = {
   logo,
+  search,
+  person,
+  bag,
+  list,
+  dropdown,
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 max-h-15 font-medium'>
 
-        <img src={assets.logo} alt='logo' className='h-30 w-auto object-contain'/>
+        <img className='h-30 w-auto object-contain' src={assets.logo} alt='logo' />
 
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
             <NavLink to={"/"} className='flex flex-col items-center gap-1'>
@@ -27,9 +28,40 @@ const Navbar = () => {
             </NavLink>
 
         </ul>
+        <div className='flex items-center gap-6'>
+            <img className='min-w-5 w-5 cursor-pointer' src={assets.search}  alt='search' />
+        
+
+            <div className='group relative'>
+                <img className='min-w-7 w-7 cursor-pointer' src={assets.person} alt='person'/>
+
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
+                    </div>
+                </div>
+            </div>
+            <Link to={"/cart"} className='relative'>
+                <img className='w-5 min-w-5' src={assets.bag} alt='bag'  />
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+            </Link>
+            <img onClick={()=>setVisible(true)} className='w-5 cursor-pointer sm:hidden'src={assets.list} alt='list' />
+        </div>
+
+        {/* Sidebar menu for small scren */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'} `}>
+            <div className='flex flex-col text-gray-600'>
+                <div onClick={()=>setVisible(false)} className='flex items-center gap-2 p-3 cursor-pointer'>
+                    <img className='h-4 rotate-180' src={assets.dropdown} alt="dropdown" />
+                    <p>Back</p>
+                </div>'
+            </div>
+        </div>
 
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
