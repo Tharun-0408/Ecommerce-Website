43fa9d9cb91939f2a8911f744a6bc6ba15c0e0e8
Tharun Reddy <tharun.guntuka0408@gmail.com>
2025-09-23 01:22:19 +0530

    added images

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -19,6 +19,27 @@ import p_7 from "./p_7.png";
 import p_8 from "./p_8.png";
 import p_9 from "./p_9.png";
 import p_10 from "./p_10.png";
+import p_11 from "./p_11.png";
+import p_12 from "./p_12.png";
+import p_13 from "./p_13.png";
+import p_14 from "./p_14.png";
+import p_15 from "./p_15.png";
+import p_16 from "./p_16.png";
+import p_17 from "./p_17.png";
+import p_18 from "./p_18.png";
+import p_19 from "./p_19.png";
+import p_20 from "./p_20.png";
+import p_21 from "./p_21.png";
+import p_22 from "./p_22.png";
+import p_23 from "./p_23.png";
+import p_24 from "./p_24.png";
+import p_25 from "./p_25.png";
+import p_26 from "./p_26.png";
+import p_27 from "./p_27.png";
+import p_28 from "./p_28.png";
+import p_29 from "./p_29.png";
+import p_30 from "./p_30.png";
+
 
 
 
@@ -163,7 +184,7 @@ export const products = [
     name: "Naruto Akatsuki Cap",
     description: "Adjustable cap with Akatsuki red cloud embroidery",
     price: 599,
-    image: "",
+    image: [p_11],
     category: "Anime",
     subCategory: "Caps",
     sizes: [],
@@ -175,7 +196,7 @@ export const products = [
     name: "One Punch Man Saitama Poster",
     description: "Premium wall poster featuring Saitama",
     price: 299,
-    image: "",
+    image: [p_12],
     category: "Anime",
     subCategory: "Posters",
     sizes: [],
@@ -187,7 +208,7 @@ export const products = [
     name: "Chainsaw Man Pochita Plush",
     description: "Adorable plush toy of Pochita",
     price: 1099,
-    image: "",
+    image: [p_13],
     category: "Anime",
     subCategory: "Plushies",
     sizes: [],
@@ -199,7 +220,7 @@ export const products = [
     name: "Death Note Notebook Replica",
     description: "Replica notebook styled after Death Note",
     price: 399,
-    image: "",
+    image: [p_14],
     category: "Anime",
     subCategory: "Collectibles",
     sizes: [],
@@ -211,7 +232,7 @@ export const products = [
     name: "Attack on Titan Eren Yeager Hoodie",
     description: "Hoodie with Eren Titan form print",
     price: 1599,
-    image: "",
+    image: [p_15],
     category: "Anime",
     subCategory: "Hoodies",
     sizes: ["S", "M", "L", "XL"],
@@ -223,7 +244,7 @@ export const products = [
     name: "My Hero Academia Todoroki Keychain",
     description: "Keychain featuring Shoto Todoroki",
     price: 199,
-    image: "",
+    image: [p_16],
     category: "Anime",
     subCategory: "Accessories",
     sizes: [],
@@ -235,7 +256,7 @@ export const products = [
     name: "Naruto Ramen Bowl Set",
     description: "Ceramic ramen bowl with chopsticks",
     price: 599,
-    image: "",
+    image: [p_17],
     category: "Anime",
     subCategory: "Kitchenware",
     sizes: [],
@@ -247,7 +268,7 @@ export const products = [
     name: "Dragon Ball Z Vegeta T-Shirt",
     description: "Cotton t-shirt featuring Prince Vegeta",
     price: 849,
-    image: "",
+    image: [p_18],
     category: "Anime",
     subCategory: "T-Shirts",
     sizes: ["S", "M", "L", "XL"],
@@ -259,7 +280,7 @@ export const products = [
     name: "Tokyo Revengers Poster Set",
     description: "Set of 3 posters featuring main characters",
     price: 499,
-    image: "",
+    image: [p_19],
     category: "Anime",
     subCategory: "Posters",
     sizes: [],
@@ -271,7 +292,7 @@ export const products = [
     name: "Demon Slayer Inosuke Boar Mask Replica",
     description: "Replica mask for cosplay of Inosuke Hashibira",
     price: 1799,
-    image: "",
+    image: [p_20],
     category: "Anime",
     subCategory: "Collectibles",
     sizes: [],
@@ -283,7 +304,7 @@ export const products = [
     name: "Naruto Shippuden Hoodie",
     description: "Officially styled Naruto Shippuden Akatsuki cloud hoodie",
     price: 1499,
-    image: "",
+    image: [p_21],
     category: "Anime",
     subCategory: "Hoodies",
     sizes: ["S", "M", "L", "XL"],
@@ -295,7 +316,7 @@ export const products = [
     name: "One Piece Straw Hat Crew T-Shirt",
     description: "Crew logo printed cotton t-shirt for One Piece fans",
     price: 799,
-    image: "",
+    image: [p_22],
     category: "Anime",
     subCategory: "T-Shirts",
     sizes: ["S", "M", "L", "XL"],
@@ -307,7 +328,7 @@ export const products = [
     name: "Attack on Titan Survey Corps Jacket",
     description: "Survey Corps emblem jacket inspired by Attack on Titan",
     price: 1999,
-    image: "",
+    image: [p_23],
     category: "Anime",
     subCategory: "Jackets",
     sizes: ["S", "M", "L", "XL"],
@@ -319,7 +340,7 @@ export const products = [
     name: "Demon Slayer Nezuko Figurine",
     description: "High-quality collectible figurine of Nezuko Kamado",
     price: 2599,
-    image: "",
+    image: [p_24],
     category: "Anime",
     subCategory: "Figurines",
     sizes: [],
@@ -331,7 +352,7 @@ export const products = [
     name: "Jujutsu Kaisen Gojo Satoru Poster",
     description: "Premium matte finish poster of Gojo Satoru",
     price: 299,
-    image: "",
+    image: [p_25],
     category: "Anime",
     subCategory: "Posters",
     sizes: [],
@@ -343,7 +364,7 @@ export const products = [
     name: "My Hero Academia Deku Keychain",
     description: "Acrylic keychain featuring Izuku Midoriya",
     price: 199,
-    image: "",
+    image: [p_26],
     category: "Anime",
     subCategory: "Accessories",
     sizes: [],
@@ -355,7 +376,7 @@ export const products = [
     name: "Tokyo Revengers Mikey Hoodie",
     description: "Soft cotton hoodie with Mikey graphic print",
     price: 1399,
-    image: "",
+    image: [p_27],
     category: "Anime",
     subCategory: "Hoodies",
     sizes: ["S", "M", "L", "XL"],
@@ -367,7 +388,7 @@ export const products = [
     name: "Spy x Family Anya Mug",
     description: "Ceramic mug with cute Anya Forger print",
     price: 349,
-    image: "",
+    image: [p_28],
     category: "Anime",
     subCategory: "Mugs",
     sizes: [],
@@ -379,7 +400,7 @@ export const products = [
     name: "Dragon Ball Z Super Saiyan T-Shirt",
     description: "Vibrant Goku Super Saiyan print on cotton t-shirt",
     price: 849,
-    image: "",
+    image: [p_29],
     category: "Anime",
     subCategory: "T-Shirts",
     sizes: ["S", "M", "L", "XL"],
@@ -391,250 +412,10 @@ export const products = [
     name: "Chainsaw Man Denji Plush Toy",
     description: "Soft plush toy of Denji with chainsaw head",
     price: 1199,
-    image: "",
+    image: [p_30],
     category: "Anime",
     subCategory: "Plushies",
     sizes: [],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p31",
-    name: "Fullmetal Alchemist Pocket Watch",
-    description: "Replica pocket watch from Fullmetal Alchemist",
-    price: 999,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p32",
-    name: "Hunter x Hunter Killua Hoodie",
-    description: "Hoodie featuring Killua Zoldyck graphic print",
-    price: 1399,
-    image: "",
-    category: "Anime",
-    subCategory: "Hoodies",
-    sizes: ["S", "M", "L", "XL"],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p33",
-    name: "Death Note L Logo Cap",
-    description: "Adjustable cap with embroidered L logo",
-    price: 599,
-    image: "",
-    category: "Anime",
-    subCategory: "Caps",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p34",
-    name: "Bleach Ichigo Sword Replica",
-    description: "Mini replica of Ichigo’s Zangetsu sword",
-    price: 1799,
-    image: "",
-    category: "Anime",
-    subCategory: "Collectibles",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p35",
-    name: "Demon Slayer Tanjiro Earrings",
-    description: "Cosplay earrings inspired by Tanjiro Kamado",
-    price: 299,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p36",
-    name: "One Piece Wanted Poster Set",
-    description: "Set of 5 wanted posters of Straw Hat crew",
-    price: 499,
-    image: "",
-    category: "Anime",
-    subCategory: "Posters",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p37",
-    name: "Naruto Kakashi Phone Case",
-    description: "Durable phone case with Kakashi Hatake print",
-    price: 399,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p38",
-    name: "Attack on Titan Wall Scroll",
-    description: "Large wall scroll featuring Eren Yeager",
-    price: 699,
-    image: "",
-    category: "Anime",
-    subCategory: "Posters",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p39",
-    name: "My Hero Academia Bakugo Figurine",
-    description: "Detailed Bakugo action figure collectible",
-    price: 2299,
-    image: "",
-    category: "Anime",
-    subCategory: "Figurines",
-    sizes: [],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p40",
-    name: "Jujutsu Kaisen Sukuna Hoodie",
-    description: "Comfortable hoodie with Sukuna curse marks",
-    price: 1599,
-    image: "",
-    category: "Anime",
-    subCategory: "Hoodies",
-    sizes: ["S", "M", "L", "XL"],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p41",
-    name: "Bleach Ichigo Bankai Hoodie",
-    description: "Cozy fleece hoodie featuring Ichigo’s Bankai mask",
-    price: 1499,
-    image: "",
-    category: "Anime",
-    subCategory: "Hoodies",
-    sizes: ["S","M","L","XL"],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p42",
-    name: "Fullmetal Alchemist State Alchemist T-Shirt",
-    description: "Soft cotton tee with the State Alchemist pocket emblem",
-    price: 899,
-    image: "",
-    category: "Anime",
-    subCategory: "T-Shirts",
-    sizes: ["S","M","L","XL"],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p43",
-    name: "One Piece Wanted Poster Set",
-    description: "Set of 5 high-quality Straw Hat crew wanted posters",
-    price: 599,
-    image: "",
-    category: "Anime",
-    subCategory: "Posters",
-    sizes: ["One Size"],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p44",
-    name: "Naruto Leaf Village Cap",
-    description: "Adjustable cap with embroidered Hidden Leaf Village symbol",
-    price: 499,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: ["One Size"],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p45",
-    name: "Dragon Ball Z Goku Figure",
-    description: "6-inch PVC action figure of Goku in Super Saiyan form",
-    price: 1199,
-    image: "",
-    category: "Anime",
-    subCategory: "Figures",
-    sizes: ["One Size"],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p46",
-    name: "Hunter x Hunter Killua Skateboard Deck",
-    description: "Durable skateboard deck featuring Killua artwork",
-    price: 1799,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: ["One Size"],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p47",
-    name: "Attack on Titan Levi Cloak",
-    description: "Green Survey Corps cloak like the one worn by Levi",
-    price: 1399,
-    image: "",
-    category: "Anime",
-    subCategory: "Jackets",
-    sizes: ["S","M","L","XL"],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p48",
-    name: "Demon Slayer Nezuko Canvas Tote Bag",
-    description: "Spacious tote bag with Nezuko chibi print",
-    price: 549,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: ["One Size"],
-    date: 1716634345448,
-    bestseller: false
-  },
-  {
-    _id: "p49",
-    name: "My Hero Academia All Might Keychain Set",
-    description: "Pack of 3 rubber keychains featuring All Might poses",
-    price: 299,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: ["One Size"],
-    date: 1716634345448,
-    bestseller: true
-  },
-  {
-    _id: "p50",
-    name: "Jujutsu Kaisen Gojo Satoru Sunglasses",
-    description: "Stylish replica sunglasses inspired by Gojo’s look",
-    price: 699,
-    image: "",
-    category: "Anime",
-    subCategory: "Accessories",
-    sizes: ["One Size"],
     date: 1716634345448,
     bestseller: false
   }
